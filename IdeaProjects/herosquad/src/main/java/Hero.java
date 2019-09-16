684932d8178be06271067f5c7e8ee2d4cdfb545c
Diane Ujeneza <dianesis17@gmail.com>
2019-09-16 11:54:16 +0200

    assigned necassary value for heros information

--- IdeaProjects/herosquad/src/main/java/Hero.java
+++ IdeaProjects/herosquad/src/main/java/Hero.java
@@ -1,3 +1,10 @@
+import java.util.ArrayList;
+
 public class Hero {
-private String
+    private String heroName;
+    private String strength;
+    private String weakness;
+    private int heroID;
+    private int age;
+    private static ArrayList<Hero> instances = new ArrayList<>();
 }
