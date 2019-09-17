0a90996f397cf82904cbdf0d454e5a0ed53bbce7
Diane Ujeneza <dianesis17@gmail.com>
2019-09-17 11:35:19 +0200

    removed unnecessary comments

--- IdeaProjects/herosquad/src/main/java/App.java
+++ IdeaProjects/herosquad/src/main/java/App.java
@@ -1,17 +1,9 @@
 import java.util.*;
-//import models.*;
 import static spark.Spark.*;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 public class App {
-//    static int getHerokuAssignedPort() {
-//        ProcessBuilder processBuilder = new ProcessBuilder();
-//        if (processBuilder.environment().get("PORT") != null) {
-//            return Integer.parseInt(processBuilder.environment().get("PORT"));
-//        }
-//        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
-//    }
     public static void main(String[] args) {
         staticFileLocation("/public");
 
@@ -24,11 +16,6 @@ public class App {
             port = 4567;
         }
         port(port);
-//
-//        staticFileLocation("/public");
-//        Hero.setUpNewHero();
-//        Hero.setUpNewHero1();
-//        Squad.setUpNewSquad();
 
         get("/", (request, response) -> {
             HashMap<String, Object> model = new HashMap<>();
